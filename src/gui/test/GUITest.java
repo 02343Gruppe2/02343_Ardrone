6a9cfc07a01126ad3a042a2a7dcc957b01a1684d
Kristin Hansen <maagenator@gmail.com>
2016-03-02 14:46:55 +0100

    Hehe...

--- src/gui/test/GUITest.java
+++ src/gui/test/GUITest.java
@@ -1,50 +1,19 @@
 package gui.test;
 
 import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 import de.yadrone.base.ARDrone;
 import de.yadrone.base.IARDrone;
-import de.yadrone.base.command.VideoChannel;
 import de.yadrone.base.exception.ARDroneException;
 import de.yadrone.base.exception.IExceptionListener;
 import de.yadrone.base.video.ImageListener;
 import gui.SpaceXGUI;
 
 public class GUITest {
-	IARDrone drone;
-	
-	public GUITest() {
-		createAndShowGUI();
-		
-		drone = new ARDrone();
-		drone.addExceptionListener(new IExceptionListener() {
-			public void exeptionOccurred(ARDroneException exc)
-			{
-				exc.printStackTrace();
-			}
-		});
-		
-		drone.start();
-		
-		drone.getVideoManager().addImageListener(new ImageListener() {
-            public void imageUpdated(BufferedImage newImage) {
-            	SpaceXGUI.updateImage(newImage);
-            }
-        });
-	}
-	
 	public static void main(String[] args) {
-		new GUITest();
-	}
-	
-	public void createAndShowGUI() {
 		// Create window
 		JFrame f = new JFrame("Space X");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,5 +28,33 @@ public class GUITest {
         // Draw the window
         f.pack();
         f.setVisible(true);
+		
+        // Instantiate drone and sync with GUI
+		IARDrone drone = null;
+		
+		try {
+			drone = new ARDrone();
+			drone.addExceptionListener(new IExceptionListener() {
+				public void exeptionOccurred(ARDroneException e) {
+					e.printStackTrace();
+				}
+			});
+			
+			drone.start();
+			
+			drone.getVideoManager().addImageListener(new ImageListener() {
+	            public void imageUpdated(BufferedImage newImage) {
+	            	SpaceXGUI.updateImage(newImage);
+	            }
+	        });
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			if (drone != null)
+				drone.stop();
+
+			System.exit(0);
+		}
 	}
 }
