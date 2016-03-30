951233449171bd1199957bfe699fd8f376367cbc
bakhoj <anders2108@gmail.com>
2016-03-30 15:15:38 +0200

    hhhhhhhhh

--- src/core/FlightAlgo.java
+++ src/core/FlightAlgo.java
@@ -4,7 +4,7 @@ import de.yadrone.base.command.CommandManager;
 
 /**
  * 
- * @author Anders Bækhøj Larsen
+ * @author Anders Bï¿½khï¿½j Larsen
  * @author Malte Tais Magnussen
  *
  */
@@ -13,6 +13,8 @@ public class FlightAlgo {
 	int [] hulaHoop = new int[4];
 	int adjustmentTolerance = 0;
 	int forwardControl = 0;
+	int doTime = 1000;
+	int sleepTime = 1000;
 	
 	IARDrone drone = null;
 	CommandManager cmd;
@@ -30,17 +32,17 @@ public class FlightAlgo {
 		
 		if((hulaHoop[0] + hulaHoop[1]) > (0 + adjustmentTolerance)) {
 			//flyv ned
-			System.out.println("Flyver ned fordi anders mor vil ha den ned i sin hals");
+			System.out.println("Gonna get low low low low low!");
 			System.out.println(hulaHoop[0]);
-			cmd.up(30).doFor(1000);
-			Thread.sleep(1000);
+			cmd.up(30).doFor(doTime);
+			Thread.sleep(sleepTime);
 			cmd.hover();
 		}
 		else if((hulaHoop[0] + hulaHoop[1]) < (0 - adjustmentTolerance)) {
 			//flyv op
-			System.out.println("Flyver op fordi anders mor vil ha den op i røven");
-			cmd.down(30).doFor(1000);
-			Thread.sleep(1000);
+			System.out.println("Because i got high!");
+			cmd.down(30).doFor(doTime);
+			Thread.sleep(sleepTime);
 			cmd.hover();
 		}
 		else {
@@ -48,17 +50,17 @@ public class FlightAlgo {
 		}
 		
 		if((hulaHoop[2] + hulaHoop[3]) > (0 + adjustmentTolerance)) {
-			//flyv højre
-			System.out.println("Flyver til højre fordi fuck anders");
-			cmd.goRight(30).doFor(1000);
-			Thread.sleep(1000);
+			//flyv hï¿½jre
+			System.out.println("You spin med right round");
+			cmd.goRight(30).doFor(doTime);
+			Thread.sleep(sleepTime);
 			cmd.hover();
 		}
 		else if((hulaHoop[2] + hulaHoop[3]) < (0 - adjustmentTolerance)) {
 			// flyv venstre
-			System.out.println("Flyver til venstre fordi anders mor best kan li at få den i røven");
-			cmd.goLeft(30).doFor(1000);
-			Thread.sleep(100);
+			System.out.println("everything you own in the box to the left");
+			cmd.goLeft(30).doFor(doTime);
+			Thread.sleep(sleepTime);
 			cmd.hover();
 		}
 		else {
@@ -67,9 +69,9 @@ public class FlightAlgo {
 		
 		if (forwardControl == 0) {
 			//flyv frem;
-			System.out.println("Flyver frem fordi Anders er grim");
-			cmd.forward(30).doFor(1000);
-			Thread.sleep(1000);
+			System.out.println("Run niggauh Run niggauh!");
+			cmd.forward(30).doFor(doTime);
+			Thread.sleep(sleepTime);
 			cmd.hover();
 		}
 		Thread.sleep(2000);
