b6853f109dedbf347d4a3815a69a29cc5235fd76
Malte <Malte@DESKTOP-SLJ9UVI>
2016-03-30 15:23:37 +0100

    Algo shiaaaaaaaaaaaaat

--- src/core/FlightAlgo.java
+++ src/core/FlightAlgo.java
@@ -1,6 +1,8 @@
 package core;
+
 import de.yadrone.base.IARDrone;
 import de.yadrone.base.command.CommandManager;
+import gui.SpaceXGUI;
 
 /**
  * 
@@ -10,7 +12,7 @@ import de.yadrone.base.command.CommandManager;
  */
 
 public class FlightAlgo {
-	int [] hulaHoop = new int[4];
+	int [] hulaHoop = setHulaHoop(100, -200, 50, -250);
 	int adjustmentTolerance = 0;
 	int forwardControl = 0;
 	int doTime = 1000;
@@ -27,12 +29,9 @@ public class FlightAlgo {
 	public void assignment1 () throws InterruptedException {
 		forwardControl = -2;
 		
-		//hulaHoop(high, low, right, left)
-		hulaHoop(100, -200, 50, -250);
-		
 		if((hulaHoop[0] + hulaHoop[1]) > (0 + adjustmentTolerance)) {
 			//flyv ned
-			System.out.println("Gonna get low low low low low!");
+			SpaceXGUI.getInstance().appendToConsole("Gonna get low low low low low!");
 			System.out.println(hulaHoop[0]);
 			cmd.up(30).doFor(doTime);
 			Thread.sleep(sleepTime);
@@ -40,7 +39,7 @@ public class FlightAlgo {
 		}
 		else if((hulaHoop[0] + hulaHoop[1]) < (0 - adjustmentTolerance)) {
 			//flyv op
-			System.out.println("Because i got high!");
+			SpaceXGUI.getInstance().appendToConsole("Because i got high!");
 			cmd.down(30).doFor(doTime);
 			Thread.sleep(sleepTime);
 			cmd.hover();
@@ -50,15 +49,15 @@ public class FlightAlgo {
 		}
 		
 		if((hulaHoop[2] + hulaHoop[3]) > (0 + adjustmentTolerance)) {
-			//flyv hï¿½jre
-			System.out.println("You spin med right round");
+			//flyv højre
+			SpaceXGUI.getInstance().appendToConsole("You spin med right round");
 			cmd.goRight(30).doFor(doTime);
 			Thread.sleep(sleepTime);
 			cmd.hover();
 		}
 		else if((hulaHoop[2] + hulaHoop[3]) < (0 - adjustmentTolerance)) {
 			// flyv venstre
-			System.out.println("everything you own in the box to the left");
+			SpaceXGUI.getInstance().appendToConsole("everything you own in the box to the left");
 			cmd.goLeft(30).doFor(doTime);
 			Thread.sleep(sleepTime);
 			cmd.hover();
@@ -69,7 +68,7 @@ public class FlightAlgo {
 		
 		if (forwardControl == 0) {
 			//flyv frem;
-			System.out.println("Run niggauh Run niggauh!");
+			SpaceXGUI.getInstance().appendToConsole("Run niggauh Run niggauh!");
 			cmd.forward(30).doFor(doTime);
 			Thread.sleep(sleepTime);
 			cmd.hover();
@@ -87,7 +86,7 @@ public class FlightAlgo {
 		
 	}
 
-	int [] hulaHoop (int high, int low, int right, int left) {
+	int [] setHulaHoop (int high, int low, int right, int left) {
 		int [] tmp = new int[4];
 		
 		tmp[0] = high;
