cc5c93e130415b29645c1659cf4c82fc47658a6a
Nalsos <Nalsos@Asylum>
2016-04-20 11:21:09 +0200

    rect problems

--- src/core/PicAnal.java
+++ src/core/PicAnal.java
@@ -91,7 +91,10 @@ public class PicAnal {
 		}
 	}
 	
-	public static void findRecs() {
+	public static ArrayList<String> findRecs() {
+		ArrayList<String> QRres = new ArrayList<String>(); 
+		boolean QRexist = false;
+		
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
 		//image from drone cam
 		/*img = SpaceXGUI.getInstance().getVPanel().getImg();
@@ -100,9 +103,16 @@ public class PicAnal {
 		//create Mat from byte[]
 		Mat imageMat = new Mat(img.getHeight(),img.getWidth(),CvType.CV_8UC3);
 		imageMat.put(0, 0, pixels);*/
-
-		//image from local file
-		Mat imageMat = Imgcodecs.imread("materials\\06.png");
+		
+		for(int k = 11; k<37; k++){
+			QRres = new ArrayList<String>(); 
+			String filename = "materials\\";
+			String saveFile = "materials\\output";
+			filename += k+".png";
+			saveFile +=k+".png";
+			
+		
+		Mat imageMat = Imgcodecs.imread(filename);
 		
 		//create grayscale, blur, canny and dilate
 		Mat grayImg = new Mat();
@@ -120,7 +130,7 @@ public class PicAnal {
 		Imgproc.findContours(grayImg, contours, hierarchy, mode, method);
 		Scalar scalarColorB = new Scalar(0,255,0); // green scalar
 		List<Rect> rects = new ArrayList<Rect>();
-		System.out.println(contours.size());
+		//System.out.println(contours.size());
 		for(int i = 0;i <contours.size();i++) {
 			MatOfPoint2f outArray = new  MatOfPoint2f();
 			Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), outArray, 5, true);
@@ -136,15 +146,41 @@ public class PicAnal {
 				qr.get(0, 0,data);
 				image.getRaster().setDataElements(0, 0, qr.cols(),qr.rows(), data);
 				//image.getRaster().setDataElements(0, 0, qr.get(0, 0));
-				lookForQr(image);
+				String QRtext = lookForQr(image);
+				
+				if(!QRtext.isEmpty()){
+					QRexist =false;
+					System.out.println(QRtext);
+					for(int z =0; z < QRres.size();z++){
+						
+						if(QRtext.equals(QRres.get(z))){
+							
+							QRexist = true;
+							
+						}
+								
+					}
+					if (QRexist == false){
+						QRres.add(QRtext);
+					}
+				}
+				
+				
+				
 			}
 			
 		}
 		Imgcodecs.imwrite("materials\\08.png",grayImg);
-		Imgcodecs.imwrite("materials\\07.png",imageMat);
+		Imgcodecs.imwrite(saveFile,imageMat);
+		for(int i = 0; i < QRres.size(); i++){
+			System.out.println(k+" "+QRres.get(i));
+		}
+		}
+		return QRres;
 	}
 	
-	public static void lookForQr(BufferedImage image) {
+	public static String lookForQr(BufferedImage image) {
+		String res = "";
 		
 		LuminanceSource source = new BufferedImageLuminanceSource(image);
 		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
@@ -153,109 +189,18 @@ public class PicAnal {
 		MultiFormatReader reader = new MultiFormatReader();
 		try {
 			Result scanResult = reader.decode(bitmap);
-			String res =scanResult.getText();
-			System.out.println(res);
+			res =scanResult.getText();
+			//System.out.println(res);
 			
 		} catch (NotFoundException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
 		}
+		return res;
 	}
 	
 	
-	public static void findRecsv2() {
-		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-		Scalar scalarColorB = new Scalar(0,255,0); // black scalar
-		//image from drone cam
-		/*img = SpaceXGUI.getInstance().getVPanel().getImg();
-		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-		
-		//create Mat from byte[]
-		Mat imageMat = new Mat(img.getHeight(),img.getWidth(),CvType.CV_8UC3);
-		imageMat.put(0, 0, pixels);*/
 
-		//image from local file
-		Mat imageMat = Imgcodecs.imread("materials\\06.png");
-		//create grayscale and blur
-		Mat grayImg = new Mat(imageMat.rows(),imageMat.cols(),imageMat.type());
-		Mat blurred = new Mat(imageMat.rows(),imageMat.cols(),imageMat.type());
-		Mat	shapes = new Mat();
-		Imgproc.GaussianBlur(imageMat, blurred, new Size(3,3),0,0);
-		Imgproc.cvtColor(blurred, grayImg, Imgproc.COLOR_BGRA2GRAY); 
-        Imgproc.Canny(grayImg, shapes, 30, 360);
-        //Imgproc.dilate(shapes, shapes, new Mat());
-        Imgcodecs.imwrite("matertials\\09.png", shapes);
-        double[] vec = new double[4];
-        Imgproc.HoughLinesP(shapes, shapes, 1, Math.PI/180, 50, 100, 100 );
-        List<Point> points = new ArrayList<Point>();
-		for (int i = 0; i<shapes.rows();i++) {
-			
-			vec = shapes.get(i, 0);
-			double x1 = vec[0];
-			double y1 = vec[1];
-			double x2 = vec[2];
-			double y2 = vec[3];
-			
-			Line2D.Double line1 = new Line2D.Double(x1, y1, x2, y2);
-			List<Line2D> lines = new ArrayList<Line2D>();
-			for(int z = i+1; z < shapes.rows();z++) {
-				
-				vec = shapes.get(z, 0);
-				double x3 = vec[0];
-				double y3 = vec[1];
-				double x4 = vec[2];
-				double y4 = vec[3];
-				Line2D.Double line2 = new Line2D.Double(x3, y3, x4, y4);
-				double angle = angleBetween2Lines(line1, line2);
-				if(Math.toDegrees(angle) < 95 && Math.toDegrees(angle) > 85 ) {
-					lines.add(line2);
-					if(lines.size() > 1) {
-						
-					}
-					//if(Line2D.linesIntersect(x1, y1, x2, y2, x3, y3, x4, y4)) {
-						//Imgproc.line(imageMat, new Point(vec[0],vec[1]), new Point(vec[2],vec[3]), scalarColorB,2);
-					//}
-					
-				    double denom = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
-				    Point intersect = new Point(((x1*y2 - y1*x2)*(x3 - x4) -
-					                       (x1 - x2)*(x3*y4 - y3*x4)) / denom,
-					                      ((x1*y2 - y1*x2)*(y3 - y4) -
-					                       (y1 - y2)*(x3*y4 - y3*x4)) / denom); 
-				    points.add(intersect);
-				    Imgproc.circle(imageMat, intersect, 2, scalarColorB);
-				/*	double a1 = (y2-y1)/(x2-x1);
-					double b1 = y1 - a1*x1 ;
-					double a2 = (y4-y3)/(x4-x3);
-					double b2 = y3 - a2*x3;
-					
-					if(x1 == x2 || x3==x4 ){
-						if(x1 != x3){
-							//paralle linjer skal ignores
-						}
-						else if(x1 == x3){
-							if(y1 == y2 || y3 == y4) {
-								
-							} else if ()
-						
-						}
-					}*/
-				}
-			}
-		}
-		
-		System.out.println(shapes.rows());
-		System.out.println(points.size());
-		Imgcodecs.imwrite("materials\\08.png",grayImg);
-		Imgcodecs.imwrite("materials\\07.png",imageMat);
-	}
-	public static double angleBetween2Lines(Line2D line1, Line2D line2)
-	{
-	    double angle1 = Math.atan2(line1.getY1() - line1.getY2(),
-	                               line1.getX1() - line1.getX2());
-	    double angle2 = Math.atan2(line2.getY1() - line2.getY2(),
-	                               line2.getX1() - line2.getX2());
-	    return angle1-angle2;
-	}
 	
 }
 
