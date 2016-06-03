fafc8437362195492e9df0ff7b62cbab89f30565
bakhoj <anders2108@gmail.com>
2016-06-03 12:28:12 +0200

    GeneralMotorCon init

--- src/algo/GeneralMotorCon.java
+++ src/algo/GeneralMotorCon.java
@@ -1,5 +1,70 @@
 package algo;
 
-public class GeneralMotorCon {
+import de.yadrone.base.ARDrone;
+import de.yadrone.base.command.CommandManager;
 
+/**
+ * @author Anders Bækhøj Larsen
+ *
+ */
+public class GeneralMotorCon {
+	private static GeneralMotorCon ourInstance = new GeneralMotorCon();
+	ARDrone drone;		//The drone object (might be unused because of CommandManager)
+	CommandManager cmd;	//The CommandManager for the drone command
+	int speed = 10;		//The speed the drone will move with
+	int time90 = 1500;	//The time for the drone to spin 90degress with given speed, TODO test the time.
+	
+	private GeneralMotorCon() {
+		drone = null;
+		cmd = null;
+	}
+	
+	/**
+	 * Should be called before once before any other method is called
+	 * @param drone
+	 */
+	public void setDrone(ARDrone drone) {
+		this.drone = drone;
+		this.cmd = this.drone.getCommandManager();
+	}
+	
+	public GeneralMotorCon getInstance(){
+		return ourInstance;
+	}
+	
+	public void forward(int time) throws InterruptedException{
+		cmd.forward(speed).doFor(time);
+		//Thread.sleep(time);
+		cmd.hover();
+	}
+	
+	public void backward(int time) throws InterruptedException {
+		cmd.backward(speed).doFor(time);
+		//Thread.sleep(time);
+		cmd.hover();
+		
+		drone.spinLeft();
+		drone.spinRight();
+	}
+	
+	public void takeoff() throws InterruptedException {
+		cmd.flatTrim();
+		Thread.sleep(500);
+		cmd.takeOff().doFor(2000);
+		//Thread.sleep(2000);
+		cmd.hover();
+	}
+	
+	public void spin90Left() throws InterruptedException {
+		cmd.spinLeft(speed).doFor(time90);
+		cmd.hover();
+	}
+	
+	public void spin90Right() throws InterruptedException {
+		cmd.spinRight(speed).doFor(time90);
+		cmd.hover();
+	}
+	
 }
+
+
