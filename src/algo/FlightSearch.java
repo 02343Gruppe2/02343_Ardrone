7f79afc6191b5c8c92993e12eb1fe84100235e34
Leo <dd@dd.cc>
2016-06-09 13:17:14 +0200

    optimering af flight

--- src/algo/FlightSearch.java
+++ src/algo/FlightSearch.java
@@ -1,5 +1,11 @@
 package algo;
 
+/**
+ * 
+ * @author Jiahua Liang
+ *
+ */
+
 import java.util.ArrayList;
 
 import core.PicAnal;
@@ -8,45 +14,34 @@ import de.yadrone.base.command.CommandManager;
 import gui.SpaceXGUI;
 
 public class FlightSearch {
-	boolean squareFound = false;
 	boolean qrcodeFound = false;
 	boolean map = false;
-	boolean forwardcontrol = false;
-	//String[][] grid = new String[963][1078];
+	boolean forwardControl = false;
+	// String[][] grid = new String[963][1078];
 
-	/*public void FlyMapping() {
+	/*
+	 * public void FlyMapping() {
+	 * 
+	 * // Wall 0 grid[188][23] = "W000.00"; grid[338][28] = "W00.01";
+	 * grid[515][23] = "W00.02"; grid[694][28] = "W00.03"; grid[840][23] =
+	 * "W00.04";
+	 * 
+	 * // Wall 1 grid[926][115] = "W01.00"; grid[926][324] = "W01.01";
+	 * grid[926][566] = "W01.02"; grid[926][721] = "W01.03"; grid[926][904] =
+	 * "W01.04";
+	 * 
+	 * // Wall 2 grid[847][1064] = "W02.00"; grid[656][995] = "W02.01";
+	 * grid[420][1070] = "W02.02"; grid[350][1070] = "W02.03"; grid[150][1070] =
+	 * "W02.04";
+	 * 
+	 * // Wall 3 grid[10][997] = "W03.00"; grid[10][740] = "W03.01";
+	 * grid[10][561] = "W03.02"; grid[10][357] = "W03.03"; grid[10][108] =
+	 * "W03.04";
+	 * 
+	 * }
+	 */
 
-		// Wall 0
-		grid[188][23] = "W000.00";
-		grid[338][28] = "W00.01";
-		grid[515][23] = "W00.02";
-		grid[694][28] = "W00.03";
-		grid[840][23] = "W00.04";
-
-		// Wall 1
-		grid[926][115] = "W01.00";
-		grid[926][324] = "W01.01";
-		grid[926][566] = "W01.02";
-		grid[926][721] = "W01.03";
-		grid[926][904] = "W01.04";
-
-		// Wall 2
-		grid[847][1064] = "W02.00";
-		grid[656][995] = "W02.01";
-		grid[420][1070] = "W02.02";
-		grid[350][1070] = "W02.03";
-		grid[150][1070] = "W02.04";
-
-		// Wall 3
-		grid[10][997] = "W03.00";
-		grid[10][740] = "W03.01";
-		grid[10][561] = "W03.02";
-		grid[10][357] = "W03.03";
-		grid[10][108] = "W03.04";
-
-	}*/
-
-	public FlightSearch() throws InterruptedException {
+	public FlightSearch(){
 		PicAnal obj = new PicAnal();
 		Assignment1 assign1 = new Assignment1();
 		Object[] res;
@@ -55,42 +50,45 @@ public class FlightSearch {
 		for (int i = 0; i < 10; i++) {
 			GeneralMotorCon.getInstance().spin90Left();
 			res = obj.findQRCodes();
-			qrcode = (ArrayList<String>)((Object[]) res[0])[0];
+			qrcode = (ArrayList<String>) ((Object[]) res[0])[0];
 
 			if (!qrcode.isEmpty()) {
-				qrcodeFound = true;	
-				
+				qrcodeFound = true;
+
 				SpaceXGUI.getInstance().appendToConsole("HEJ");
 				break;
 			}
-			
+
 		}
-				
+
 		while (!qrcodeFound) {
 
 			GeneralMotorCon.getInstance().forward(2000);
 			res = obj.findQRCodes();
-			qrcode = (ArrayList<String>)((Object[]) res[0])[0];
+			qrcode = (ArrayList<String>) ((Object[]) res[0])[0];
 
 			if (!qrcode.isEmpty()) {
 				qrcodeFound = true;
-			} 
+
+			}
 		}
-		
+
 		GeneralMotorCon.getInstance().landing();
-		
-		/*while (!map) {
+
+		while (!map) {
 			res = obj.findQRCodes();
-			qrcode = (ArrayList<String>) res[0];
+			qrcode = (ArrayList<String>) ((Object[]) res[0])[0];
 
 			for (int j = 0; j < qrcode.size(); j++) {
 
-				if (qrcode.get(j).equals("W00.00")) {
+				if (assign1.updateHulahoop()) {
+					map = true;
+				} else if (qrcode.get(j).equals("W00.00")) {
 
 					// spin 180 grader
 					GeneralMotorCon.getInstance().spin90Right();
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W00.01")) {
 					GeneralMotorCon.getInstance().right();
@@ -99,7 +97,7 @@ public class FlightSearch {
 					// Spin 180 grader
 					GeneralMotorCon.getInstance().spin90Right();
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W00.03")) {
 					GeneralMotorCon.getInstance().right();
@@ -108,7 +106,7 @@ public class FlightSearch {
 					// Spin 180 grader
 					GeneralMotorCon.getInstance().spin90Right();
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W01.00")) {
 					// Spin 180 grader
@@ -137,12 +135,12 @@ public class FlightSearch {
 
 				} else if (qrcode.get(j).equals("W02.00")) {
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 				} else if (qrcode.get(j).equals("W02.01")) {
 					// Spin 180 grader
 					GeneralMotorCon.getInstance().spin90Left();
 					GeneralMotorCon.getInstance().spin90Left();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W02.02")) {
 					GeneralMotorCon.getInstance().left();
@@ -151,53 +149,52 @@ public class FlightSearch {
 					// Spin 180 grader
 					GeneralMotorCon.getInstance().spin90Left();
 					GeneralMotorCon.getInstance().spin90Left();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W02.04")) {
 					GeneralMotorCon.getInstance().left();
 
 				} else if (qrcode.get(j).equals("W03.00")) {
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W03.01")) {
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W03.02")) {
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W03.03")) {
 					GeneralMotorCon.getInstance().spin90Right();
-					forwardcontrol = true;
+					forwardControl = true;
 
 				} else if (qrcode.get(j).equals("W03.04")) {
 					GeneralMotorCon.getInstance().spin90Right();
 
-				} else if (qrcode.get(j).contains("P")) {
-					map = true;
 				}
 			}
-			while (forwardcontrol) {
+			while (forwardControl) {
 				GeneralMotorCon.getInstance().forward(2000);
 				res = obj.findQRCodes();
-				qrcode = (ArrayList<String>) res[0];
+				qrcode = (ArrayList<String>) ((Object[]) res[0])[0];
 
 				for (int g = 0; g < qrcode.size(); g++) {
-					if (qrcode.get(g).contains("P")) {
+					if (assign1.updateHulahoop()) {
+						forwardControl = false;
 						map = true;
-						forwardcontrol = false;
+
 					} else if (qrcode.get(g).contains("W")) {
-						forwardcontrol = false;
+						forwardControl = false;
 					}
 				}
 
 			}
-		}*/
+		}
 
 		// TODO ændre run to fly
-		//assign1.run();
+		// assign1.fly();
 
 	}
 }
