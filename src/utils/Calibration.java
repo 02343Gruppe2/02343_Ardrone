0d5bbef5697fe25b38d26de789d3626dd644223f
Jesper <jesperbn90@gmail.com>
2016-06-13 15:47:11 +0200

    calibration ready for test

--- src/utils/Calibration.java
+++ src/utils/Calibration.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -22,27 +25,36 @@ public class Calibration {
 	public static void main(String[] args) {
 		int xsize = 9, ysize = 6;
 		
+		List<Mat> imagePoint = new ArrayList<Mat>(), rvecs = new ArrayList<Mat>(), tvecs = new ArrayList<Mat>();
+		for(int x =0;x<xsize;x++) {
+			for(int y= 0;y<ysize;y++) {
+				MatOfPoint3f points = new MatOfPoint3f(new Point3(x,y,0));
+				imagePoint.add(points);
+			}
+		}
 		
+		List<Mat> cornerList = new ArrayList<Mat>();
 		for( int i = 0 ; i<20; i++) {
 			Mat image = Imgcodecs.imread("materials\\cal\\chessboard"+i+".png");
 			Mat grayImg = new Mat();
 			Imgproc.cvtColor(image, grayImg, Imgproc.COLOR_BGRA2GRAY);
-		/*	Mat corners = new Mat();
-			for(int x =0;x<xsize;x++) {
-				for(int y= 0;y<ysize;y++) {
-					MatOfPoint3f points = new MatOfPoint3f(new Point3(x,y,0));
-					corners.push_back(points);
-				}
-			}*/
+			
 			MatOfPoint2f corners = new MatOfPoint2f();
 			Size size = new Size(xsize, ysize);
 			boolean found = Calib3d.findChessboardCorners(image, size, corners);
 			if(found) {
 				Imgproc.cornerSubPix(grayImg, corners, new Size(11,11), new Size(-1,-1), new TermCriteria(TermCriteria.EPS+TermCriteria.MAX_ITER, 30, 0.1));
-				
+				cornerList.add(corners);
 			}
 			Calib3d.drawChessboardCorners(image, new Size(11,11), corners, found);
 			Imgcodecs.imwrite("materials\\cal\\drawnimg"+i+".png", image);
 		}
+		Mat cameraMatrix = Mat.eye(new Size(xsize,ysize), CvType.CV_64F);
+		Mat distCoeffs =  Mat.zeros(new Size(0,0), CvType.CV_64F);
+		Calib3d.calibrateCamera(cornerList, imagePoint, new Size(xsize,ysize), cameraMatrix,distCoeffs, rvecs, tvecs);
+		Mat imgIn = Imgcodecs.imread("materials\\picture_9.png");
+		
+		Imgproc.undistort(imgIn, imgIn, cameraMatrix, distCoeffs);
+		Imgcodecs.imwrite("materials\\drawming.png",imgIn);
 	}
 }
