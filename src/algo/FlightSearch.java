d53a2285c2013931ef7e2aaa40217d9e04d974d3
Leo <dd@dd.cc>
2016-06-15 12:39:52 +0200

    Balance og distance implementeret mangler test med den.

--- src/algo/FlightSearch.java
+++ src/algo/FlightSearch.java
@@ -30,33 +30,34 @@ public class FlightSearch {
 	Object[] res;
 	ArrayList<String> qrcode = null;
 
-	 /*String[][] grid = new String[963][1078];
-	 
-	  public void FlyMapping() {
-	  
-	  // Wall 0 grid[188][23] = "W000.00"; grid[338][28] = "W00.01";
-	  grid[515][23] = "W00.02"; grid[694][28] = "W00.03"; grid[840][23] =
-	  "W00.04";
-	  
-	  // Wall 1 grid[926][115] = "W01.00"; grid[926][324] = "W01.01";
-	  grid[926][566] = "W01.02"; grid[926][721] = "W01.03"; grid[926][904] =
-	  "W01.04";
-	  
-	  // Wall 2 grid[847][1064] = "W02.00"; grid[656][995] = "W02.01";
-	  grid[420][1070] = "W02.02"; grid[350][1070] = "W02.03"; grid[150][1070] =
-	  "W02.04";
-	  
-	  // Wall 3 grid[10][997] = "W03.00"; grid[10][740] = "W03.01";
-	  grid[10][561] = "W03.02"; grid[10][357] = "W03.03"; grid[10][108] =
-	  "W03.04"; 
-	  }*/
-	 
-	//QRcode scanner metode.
-	public void qrcodeScan(){
+	/*
+	 * String[][] grid = new String[963][1078];
+	 * 
+	 * public void FlyMapping() {
+	 * 
+	 * // Wall 0 grid[188][23] = "W000.00"; grid[338][28] = "W00.01";
+	 * grid[515][23] = "W00.02"; grid[694][28] = "W00.03"; grid[840][23] =
+	 * "W00.04";
+	 * 
+	 * // Wall 1 grid[926][115] = "W01.00"; grid[926][324] = "W01.01";
+	 * grid[926][566] = "W01.02"; grid[926][721] = "W01.03"; grid[926][904] =
+	 * "W01.04";
+	 * 
+	 * // Wall 2 grid[847][1064] = "W02.00"; grid[656][995] = "W02.01";
+	 * grid[420][1070] = "W02.02"; grid[350][1070] = "W02.03"; grid[150][1070] =
+	 * "W02.04";
+	 * 
+	 * // Wall 3 grid[10][997] = "W03.00"; grid[10][740] = "W03.01";
+	 * grid[10][561] = "W03.02"; grid[10][357] = "W03.03"; grid[10][108] =
+	 * "W03.04"; }
+	 */
+
+	// QRcode scanner metode.
+	public void qrcodeScan() {
 		res = obj.findQRCodes();
 		qrcode = (ArrayList<String>) ((Object[]) res[0])[0];
 	}
-	
+
 	// Metode for forward check.
 	public void forwardCheck() {
 		GeneralMotorCon.getInstance().forward(2000);
@@ -74,15 +75,54 @@ public class FlightSearch {
 			}
 		}
 	}
-	
-	public void adjustdrone(int j){
-		Rect rect = ((ArrayList<Rect>) ((Object[]) res[0])[1]).get(j);
-		double x = ((rect.br().x - rect.tl().x)/2)+ rect.tl().x, y = ((rect.br().y - rect.tl().y)/2) + rect.tl().y;
-		ImgProc.coordinateDisplacement(x, y);
-		
-		
+
+	public void adjustdrone(int j, String currentQr) {
+		int errormargin = 100, rectangleWidth = 100;
+		boolean isyadjusted = false, isxadjusted = false, newQRCode = true, distance = false;
+
+		int scanCounter = 0;
+		while (!isyadjusted && !isxadjusted && !distance) {
+			if (newQRCode && scanCounter < 5) {
+				Rect rect = ((ArrayList<Rect>) ((Object[]) res[0])[1]).get(j);
+				double x = ((rect.br().x - rect.tl().x) / 2) + rect.tl().x,
+						y = ((rect.br().y - rect.tl().y) / 2) + rect.tl().y;
+				double[] displacedCoordinates = ImgProc.coordinateDisplacement(x, y);
+				x = displacedCoordinates[0];
+				y = displacedCoordinates[1];
+				if (x > errormargin) {
+					GeneralMotorConSchedule.getInstance().right();
+				} else if (x < -errormargin) {
+					GeneralMotorConSchedule.getInstance().left();
+				} else {
+					isxadjusted = true;
+				}
+				if (y > errormargin) {
+					GeneralMotorConSchedule.getInstance().raiseAltitude();
+				} else if (y < -errormargin) {
+					GeneralMotorConSchedule.getInstance().lowerAltitude();
+				} else {
+					isyadjusted = true;
+				}
+				SpaceXGUI.getInstance().appendToConsole("\nrect str: "+rect.width);
+				if (rect.width < rectangleWidth) {
+					GeneralMotorConSchedule.getInstance().forward(100);
+				} else {
+					distance = true;
+				}
+			}
+			qrcodeScan();
+			scanCounter++;
+			newQRCode = false;
+			for (j = 0; j < qrcode.size(); j++) {
+				if (qrcode.get(j).equals(currentQr)) {
+					newQRCode = true;
+					scanCounter = 0;
+					break;
+				}
+			}
+		}
 	}
-	
+
 	public void search() {
 		// spin 10 times and scan for hulahop and qrcode
 		for (int i = 0; i < 10; i++) {
@@ -102,7 +142,7 @@ public class FlightSearch {
 		while (!qrcodeFound) {
 			// Scan for qrcode.
 			qrcodeScan();
-			
+
 			// Checking for Hulahop found
 			if (assign1.updateHulaHoop()) {
 				qrcodeFound = true;
@@ -129,7 +169,7 @@ public class FlightSearch {
 					map = true;
 					break;
 				} else if (qrcode.get(j).equals("W00.00")) {
-
+					adjustdrone(j, "W00.00");
 					// spin 180 grader if QRcode match the exactly wall.
 					GeneralMotorCon.getInstance().spin90Right();
 					GeneralMotorCon.getInstance().spin90Right();
