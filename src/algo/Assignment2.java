a208cfa0bef77ef5e6bfdb3b8d5c871539d6c7de
Malte <Malte@DESKTOP-SLJ9UVI>
2016-06-15 10:51:28 +0200

    Comment for life!

--- src/algo/Assignment2.java
+++ src/algo/Assignment2.java
@@ -1,5 +1,10 @@
 package algo;
 
+/**
+ * Assignment 2
+ * The cube assignment
+ * The drone has to map the cubes on the floor
+ */
 public class Assignment2 {
 
 }
--- src/algo/Assignment3.java
+++ src/algo/Assignment3.java
@@ -1,5 +1,10 @@
 package algo;
 
+/**
+ * Assignment 3
+ * The airfield assignment
+ * The drone has to land and fly from specific "airfields"
+ */
 public class Assignment3 {
 
 }
